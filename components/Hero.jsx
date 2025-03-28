5723569c21966c27cb1459c9d349e3563f485540
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-03-28 17:27:25 +0500

    fix background

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -78,18 +78,24 @@ export default function Hero() {
         />
       </div>
 
-      <div className="absolute bottom-0 right-0 p-[30px] 3xl:p-[50px]">
-        <img
-          src="/assets/Powered by Dominus P.svg"
-          alt="logo"
-          className="w-[122.31px] h-[30px]"
-        />
-      </div>
-
       {/* Footer */}
-      <div className="absolute bottom-0 w-full text-center text-black py-[30px] 3xl:py-[50px] text-[8px] 3xl:text-[11px] tracking-[3px]">
-        © COPYRIGHT 2025 DECORIS LIMITED. ALL RIGHTS RESERVED.
-      </div>
+      {/* Footer */}
+<div className="absolute bottom-0 w-full flex flex-col-reverse gap-[20px] sm:flex-row items-center sm:justify-between text-black p-[30px] 3xl:p-[50px] text-[8px] 3xl:text-[11px] tracking-[3px]">
+  {/* Centered text for large screens */}
+  <div className="w-full text-center sm:w-auto sm:absolute sm:left-1/2 sm:-translate-x-1/2">
+    <p>© COPYRIGHT 2025 DECORIS LIMITED. ALL RIGHTS RESERVED.</p>
+  </div>
+
+  {/* Image at the right corner */}
+  <div className="sm:absolute sm:right-[30px] 3xl:right-[50px]">
+    <img
+      src="/assets/Powered by Dominus P.svg"
+      alt="logo"
+      className="w-[122.31px] h-[30px]"
+    />
+  </div>
+</div>
+
     </div>
   );
 }
